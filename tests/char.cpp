b2d7e7a9d1fbbde0c9b7512ea326e1a7d28ca8dd
MaximLeontev <maximricardo8@gmail.com>
2026-05-10 03:02:19 +0200

    added testing func overloading with various char types

--- tests/char.cpp
+++ tests/char.cpp
@@ -1,3 +1,8 @@
+void f(char c);
+void f(wchar_t c);
+void f(char16_t c);
+void f(char32_t c);
+
 int main()
 {
     char a = 'x';
@@ -5,7 +10,13 @@ int main()
     char32_t c = U'🥀';
     wchar_t d = L'β';
 
-    // should error out for being too big
-    char err_a = '猫';
-    char16_t err_b = u'🥀';
+    f('a');  // calls 1:1
+    f(L'a'); // calls 2:1
+    f(u'a'); // calls 3:1
+    f(U'a'); // calls 4:1
+
+    f(a); // calls 1:1
+    f(b); // calls 2:1
+    f(c); // calls 3:1
+    f(d); // calls 4:1
 }
