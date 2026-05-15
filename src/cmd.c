08f9253f04ce2b01c6c90cf9f3875c12c18591ad
MaximLeontev <maximricardo8@gmail.com>
2026-05-15 20:02:05 +0200

    added a help menu

--- src/cmd.c
+++ src/cmd.c
@@ -6,9 +6,24 @@
 bool cmd_args_inited = false;
 static struct CMD_Args cmd_args;
 
+static void print_help_menu(void)
+{
+    printf("Mid C++ Compiler\n\n");
+    printf("Usage: mcppc [options] file...\n\n");
+    printf("Options:\n");
+    printf("\t-h, --help\t\tPrint the help menu\n");
+    printf("\t--ast <file>\t\tLog the AST of the compilation unit to a file\n");
+    printf("\t--log-tokens\t\tLog a list of every token in the compilation "
+           "unit\n");
+    printf("\t--log-symbols\t\tLog a list of every symbol in the compilation "
+           "unit\n");
+}
+
 void CMD_init_args(int argc, char **argv)
 {
     cmd_args = (struct CMD_Args){};
+
+    bool help = false;
 
     for (int i = 1; i < argc; ++i) {
         if (!strcmp(argv[i], "--ast"))
@@ -17,6 +32,8 @@ void CMD_init_args(int argc, char **argv)
             cmd_args.log_tokens = true;
         else if (!strcmp(argv[i], "--log-symbols"))
             cmd_args.log_symbols = true;
+        else if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help"))
+            help = true;
         else if (argv[i][0] == '-')
             fprintf(stderr, "error: unknown cmd line argument '%s'\n", argv[i]);
         else
@@ -24,6 +41,9 @@ void CMD_init_args(int argc, char **argv)
     }
 
     cmd_args_inited = true;
+
+    if (help)
+        print_help_menu();
 }
 
 const struct CMD_Args *CMD_get_args(void)
